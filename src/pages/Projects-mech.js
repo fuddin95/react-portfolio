3ce85bd33c217fe400053009c12a2605281e7326
Muhammad Uddin <fahad.uddin@mail.utoronto.ca>
2021-02-16 16:49:15 -0500

    test-imgur url link-try2

--- src/pages/Projects-mech.js
+++ src/pages/Projects-mech.js
@@ -13,20 +13,20 @@ const frontend = [
     },
     {
         id:2,
-        image:"https://lh3.googleusercontent.com/lJPt7Sh-Z-u2yXPoxwOXOFRbOg-mQlq74WCOjVuTAneBYgQTpKWF-26zPDr7739ahS4KLlB3OKwZ8vMl-7YxA6weJnFN5_rMWfn8m0YqkPxpeLPPHmSxBk4yCL4dCe7OmzwX2IOcaULgozansnPFsM-cMFA3rpVnz0_wxeB4aaSK3moAipX4zWMqjiT-3JZKtbOgicFH3IT8vMAZGgc7uAILv74QtusF2QdjEf9RQ_tkyOK6tnPEZS6Rh825yTcmmEBbSGCjKJV0OmPWD5sRDw8UOrHamyyki-sz3TB3p6CfX00imP0zuTVMwSZPjpOkfA6EhFT4_0DRA9dcgz-qqVqRPzsDI4qdFy8Ox_-dltaHkRdY3dJ-q5eEVRGMkE-yibP285N0WBP4Vd03N2LRJEdRi6o5zae_eMTWyKV_pfSTA2BZxp5lCFjnSkZ1bgRIUnva6cMDacI1rAgpapUfn2bWnl-LlAhgG7WDU94eVTtpbJtSxH7u4eVQTSEx1Sxhijlp6xWlJC9KSHStOCwdEK1wLu-wCf8Cd73XmvyhF3xl8GlszW3ppT37F4o-Xo7zuUrRuuDzsfO_IX9KdU-LEfrkknU_scPPaI121FTE-6G35UPjcKHqiM1cd327HK8AXMYnMvdLkKGRNgK2NoFnpJQnCnrTVSHX0nZBTUyzlV7gYeK3PzX6Bv6SfZZaOg=w914-h756-no?authuser=0",
+        image:"https://i.imgur.com/zV1IJ0i.png",
         title:"Pets Day Out",
         text:"A fully responsive webpage designed using Bootsrap elements such as carousel, containers, nav bar and pricing plan. Additionally, the CSS is customized to attain the desired result such as change color when mouse is hovered on the specific text.",
         url:"https://mywebsite-mfu.herokuapp.com/projects/dogCaravan/index.html"
     },
     {
         id:3,
-        image:"https://lh3.googleusercontent.com/wtI92x1KalOH_W8dm7CyWFwrAwwG2JOTVai2pMOqPGCosS0BnEK5m1lTHB3r5gtRoDA6AGRGgkVLA3iqQXTQe3Gm6a_iYao3wdayP6pk4ACvzLowIrYvB2BEVsXsXWHS6Un_xdUwLWFfVTm30pYFIAFFapikqJy2THoSYORSaMrWrXBEkO2iKQjef4D4zj78oubBbEBWLPVAnHysffsMt0FaQ2G2hxdl3Sh-59UNzftTBMRfriFAsSxujb8IwwVbAQ_64TDSe6rUk28y8mrgiln1YwHsC4jhU1Fm0bcbczuO32r9QQRufh9JVBcWZaCJNEHTWuhwjIb5sq8lEtE57fNbFFhDqh4oO1WsNC4RKohfRynjIzIWv_eTeDd3l_5hmgDrLnF_escTWtZ6nfcPFTWqzKjnlMBOYatAMykKx56l07Sje4kAibF7oUv0ABEnNx2yF866AHBprS6TJCVt4ErZUVH7xjHc-jWGjzyo-5sW5GyBcT1noKM3QSb4zyz0FF9obxBmUOkv3FOFGU9cL9-zJi7ULkWiPUgA8iYAEdQ_DooxY2jwV626TXO6xbAkZBfXDT39k_uvOv3j2riF_sIZk-Q7scPXCew07INlLOeBIntMnH2fc2AtgjAr8UTg3HoW3GyAo1hThqSNvyAfWcmQP37Rj_3AzmrqpSfkP0It2e54x_sZqnY4TmYHYg=s767-no?authuser=0",
+        image:"https://i.imgur.com/7fEWnLD.jpg",
         title:"Simon Game",
         text:"The webpage is built from scratch, it uses jQuerry and javascript to record the input of the user and compare it against the game rules. It also uses custom fonts by google fonts.",
         url:"https://mywebsite-mfu.herokuapp.com/projects/Simon%20Game/index.html"
     },
     {   id:"4",
-        image:"https://lh3.googleusercontent.com/_WCpVd3cv-HvezZ89qB-jGpl4QSGKYN6fzawsCKuKBd7_-Uh8Xzca07MGChoTFPH1IVU_8Dp9iLm1ynlnZ3CeUD9lMmpnoI_K30Iu38-k6po_fBwGk6Q00CgbWfNJYH4uruGXnAxtayNl-uvHgNCgNTGxO4BF8CdDCKuCVwouvZbr-_RZNJIJbDbdhQFh0Q2GT4ctIPGdiAH0_ighCnzvJKydr7WBvpCt11Zkp_Iu23FTKVLtqB26wULWJe6sdjuVq2wQmh2GqPrg2-XjL57O_T2yAnaYcO2GwpndYF-rIiNvL-1loynzl9heVDtB7GLWvu5eaZeqO2Zwd1_NwndG7jOuYoRtkRGN3IO1kLbavdUmHLuz3ykHyE4JP4_Yus0kc4OzdmnFu5o-ebS4CeFJNzh-9db0i0R-oQFdmsR33DNbnegHZIUBLAGpjec74mocUG0Nk7R2llTgKCBACQyWBuJ6YCFWvyHmqTo284-8S37YAsOHCm68Kctb41UvFcfof9FaMy8aAanGILjPh3AwLMhKK1k4qEYTvvRCj-e86U8pFLDwlZ7uFHlyur8wecdj1PRU4NlgY39saRpYbAm7ACP9kD7rMe7gIUUauB2wbY6PT5RB6sKFBQW8H2aEu9CLPWHxWzMGWhOwSJVKHs1rcGUQ_9AtErhFcwrufKdqR1tc6bwzggjTIpkUjfq5A=w600-h448-no?authuser=0",
+        image:"https://i.imgur.com/2K7vOjh.jpg",
         title:"Dice Game",
         text:"This webpage allows you to roll dices every time the page is refreshed The dice face and the winning player name is diplayed dynamically. The website is styled using custom CSS",
         url:"https://mywebsite-mfu.herokuapp.com/projects/Dice%20game/dicee.html"
@@ -37,11 +37,11 @@ const frontend = [
 const reactApps=[
     {
         id:1,
-        image:"",
+        image:"https://i.imgur.com/IUGl9d2.jpg",
         title:"Note Taker",
         text:"This react webapp is similar to Google's Keeper App. The notes are updated automatically using hooks and can be deleted as well respectively. The app also uses Icons and animations from Material UI.",
         url:"https://lt0x2.csb.app/"
-    }
+    },
 ]
 
 const mechanicalProjects=[
@@ -49,7 +49,7 @@ const mechanicalProjects=[
         id:1,
         image:"",
         title:"Di-Electric Test Fixture (Kinectrics)",
-        text:"<br/>",
+        text:"",
         url:""
     }
 ]
