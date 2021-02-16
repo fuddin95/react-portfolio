27c4e90c4646791d28d0f8a73e44982ecaccc2cd
Muhammad Uddin <fahad.uddin@mail.utoronto.ca>
2021-02-15 21:39:33 -0500

    buildpack installed on heroku:

--- src/components/CarouselB.jsx
+++ src/components/CarouselB.jsx
@@ -8,7 +8,7 @@ export default function CarouselB() {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://techcrunch.com/wp-content/uploads/2019/02/mechanical.jpg"
+                        src="https://images.unsplash.com/photo-1595776613215-fe04b78de7d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
                         alt="First slide"
                     />
                     <Carousel.Caption>
@@ -19,7 +19,7 @@ export default function CarouselB() {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://apluscourses.com/wp-content/uploads/2020/06/The-Evolution-Of-Web-Development.jpg"
+                        src="https://c1.wallpaperflare.com/preview/704/134/461/black-and-white-business-code-coding.jpg"
                         alt="Seond slide"
                     />
 
@@ -31,7 +31,7 @@ export default function CarouselB() {
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://www.danielhonrade.com/wp-content/uploads/2020/09/Web-development-2.jpg"
+                        src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1607&q=80"
                         alt="Third slide"
                     />
 
